9c166c336d4dfb76c7f8406c317c051c80cf41e6
CarpeDiemNan! <nanurci@gmail.com>
2018-01-20 21:29:51 -0500

    I'm so happy this is working!

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var Twitter = require('twitter');
 var inquirer = require("inquirer");
 var request = require('request'); 
 var Spotify = require('node-spotify-api');
- 
+
 // read key file which gets my unique keys from the .env file
 
 fs.readFile('keys.js', "utf8", function(error, data){
@@ -26,10 +26,41 @@ var client = new Twitter({
 			  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 			});	 
 
+// blueprint for making a spotify client to access spotify API
+
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET     
+  });
+
+//****************  SPOTIFY PROCESSES ***************************
+ 
+// command line example:
+
+// $ node liri "spotify-this-song" "all the young dudes"
 
 
-// determine if user wants to post or get a tweet
-// user should enter at command line in this format:
+if(process.argv[2] == "spotify-this-song"){
+  var spotty = process.argv[2];
+   console.log("READING ARGV[2]");
+   console.log(spotty);
+
+   // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      console.log(data.tracks.items); 
+      });
+    };
+    
+ 
+
+
+//******************  TWITTER  PROCESSES ************************
+
+// command line examples:
+
 // to post a tweet --> $ node liri "post" "Tea Time"
 // to get tweets about a subject --> $ node liri "get" "Taxes"
 
